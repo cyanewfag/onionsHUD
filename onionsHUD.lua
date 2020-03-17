0ab8e06073a3bf80d58003f09f26bc740352a35d
onion <62121953+cyanewfag@users.noreply.github.com>
2020-03-17 19:36:15 -0400

    Update onionsHUD.lua

--- onionsHUD.lua
+++ onionsHUD.lua
@@ -8,42 +8,59 @@ local onion_window = gui.Tab(gui.Reference("Settings"), 'onion_window', "Onion's
 -- Groupboxes
 local onion_window_groupbox_1 = gui.Groupbox(onion_window, 'Gradient Settings', 15, 15)
 local onion_window_groupbox_2 = gui.Groupbox(onion_window, 'HUD Settings', 15, 295)
+local onion_window_groupbox_3 = gui.Groupbox(onion_window, 'Side HUD Settings', 15, 1010)
 
 -- Checkboxes
 local onion_gradient_enabled = gui.Checkbox(onion_window_groupbox_1, 'onion_gradient_enabled', 'Enabled', true)
 local onion_gradient_vertical = gui.Checkbox(onion_window_groupbox_1, 'onion_gradient_vertical', 'Vertical Gradient (Clowns Only)', false)
 local onion_hud_enabled = gui.Checkbox(onion_window_groupbox_2, 'onion_hud_enabled', 'Enabled', true)
 local onion_hud_position_dynamic = gui.Checkbox(onion_window_groupbox_2, 'onion_hud_position_dynamic', 'Dynamic HUD Position', true)
-local onion_hud_position_drag = gui.Checkbox(onion_window_groupbox_2, 'onion_hud_position_drag', 'Draggable HUD (W.I.P)', false)
+local onion_hud_enabled_ingame = gui.Checkbox(onion_window_groupbox_2, 'onion_hud_enabled_ingame', 'Ingame Check', true)
+local onion_hud_side_enabled = gui.Checkbox(onion_window_groupbox_3, 'onion_hud_side_enabled', 'Enabled', true)
+local onion_hud_side_enabled_ingame = gui.Checkbox(onion_window_groupbox_3, 'onion_hud_side_enabled_ingame', 'Ingame Check', true)
 
 -- Sliders
 local onion_gradient_height = gui.Slider(onion_window_groupbox_1,'onion_gradient_height', 'Gradient Height', 5, 1, 20)
 local onion_hud_position_x = gui.Slider(onion_window_groupbox_2,'onion_hud_position_x', 'X Offset', 15, 0, 75)
 local onion_hud_position_y = gui.Slider(onion_window_groupbox_2,'onion_hud_position_y', 'Y Offset', 15, 0, 75)
+local onion_hud_side_position_y = gui.Slider(onion_window_groupbox_3,'onion_hud_side_position_y', 'Y Offset', 0, -200, 200)
+local onion_hud_side_position_x = gui.Slider(onion_window_groupbox_3,'onion_hud_side_position_x', 'X Offset', 15, 0, 75)
+local onion_hud_side_distance_y = gui.Slider(onion_window_groupbox_3,'onion_hud_side_distance_y', 'Vertical Distance', 10, 0, 50)
 
 -- Comboboxs
 local onion_hud_position = gui.Combobox(onion_window_groupbox_2,'onion_hud_position', 'HUD Position', "Top Right", "Top Left", "Bottom Right", "Bottom Left")
 local onion_hud_style = gui.Combobox(onion_window_groupbox_2,'onion_hud_style', 'HUD Style', "Sypher", "Skeet", "Onetap")
+local onion_hud_side_style = gui.Combobox(onion_window_groupbox_3,'onion_hud_side_style', 'Side HUD Style', "Sypher", "Skeet", "Onetap")
 
 -- Color Pickers
 local onion_gradient_col_1 = gui.ColorPicker(onion_window_groupbox_1,'onion_gradient_col_1', 'Gradient Color 1', 59, 175, 222, 255)
 local onion_gradient_col_2 = gui.ColorPicker(onion_window_groupbox_1,'onion_gradient_col_2', 'Gradient Color 2', 202, 70, 205, 255)
 local onion_gradient_col_3 = gui.ColorPicker(onion_window_groupbox_1,'onion_gradient_col_3', 'Gradient Color 3', 201, 227, 58, 255)
-local onion_hud_color_backcolor = gui.ColorPicker(onion_window_groupbox_2,'onion_hud_color_backcolor', 'HUD Backcolor', 43, 43, 43, 255)
 local onion_hud_color_bordercolor = gui.ColorPicker(onion_window_groupbox_2,'onion_hud_color_bordercolor', 'HUD Border Color', 0, 183, 255, 255)
 local onion_hud_color_bordercolor2 = gui.ColorPicker(onion_window_groupbox_2,'onion_hud_color_bordercolor2', 'HUD Border Color 2', 0, 137, 191, 255)
 local onion_hud_color_text = gui.ColorPicker(onion_window_groupbox_2,'onion_hud_color_text', 'HUD Text Color', 255, 255, 255, 255)
+local onion_hud_side_color_bordercolor = gui.ColorPicker(onion_window_groupbox_3,'onion_hud_side_color_bordercolor', 'HUD Border Color', 0, 183, 255, 255)
+local onion_hud_side_color_bordercolor2 = gui.ColorPicker(onion_window_groupbox_3,'onion_hud_side_color_bordercolor2', 'HUD Border Color 2', 0, 137, 191, 255)
+local onion_hud_side_color_text = gui.ColorPicker(onion_window_groupbox_3,'onion_hud_side_color_text', 'HUD Text Color', 255, 255, 255, 255)
 
--- Options
-local onion_window_groupbox_3 = gui.Groupbox(onion_window_groupbox_2, 'HUD Options', 0, 445)
-local onion_hud_options_username = gui.Checkbox(onion_window_groupbox_3, 'onion_hud_options_username', 'Username', true)
-local onion_hud_options_ping = gui.Checkbox(onion_window_groupbox_3, 'onion_hud_options_ping', 'Ping', true)
-local onion_hud_options_server = gui.Checkbox(onion_window_groupbox_3, 'onion_hud_options_server', 'Server', true)
-local onion_hud_options_velocity = gui.Checkbox(onion_window_groupbox_3, 'onion_hud_options_velocity', 'Velocity', true)
-local onion_hud_options_tickrate = gui.Checkbox(onion_window_groupbox_3, 'onion_hud_options_tickrate', 'Tickrate', true)
+-- HUD Options
+local onion_window_groupbox_2_groupbox_1 = gui.Groupbox(onion_window_groupbox_2, 'HUD Options', 0, 415)
+local onion_hud_options_username = gui.Checkbox(onion_window_groupbox_2_groupbox_1, 'onion_hud_options_username', 'Username', true)
+local onion_hud_options_ping = gui.Checkbox(onion_window_groupbox_2_groupbox_1, 'onion_hud_options_ping', 'Ping', true)
+local onion_hud_options_server = gui.Checkbox(onion_window_groupbox_2_groupbox_1, 'onion_hud_options_server', 'Server', true)
+local onion_hud_options_velocity = gui.Checkbox(onion_window_groupbox_2_groupbox_1, 'onion_hud_options_velocity', 'Velocity', true)
+local onion_hud_options_tickrate = gui.Checkbox(onion_window_groupbox_2_groupbox_1, 'onion_hud_options_tickrate', 'Tickrate', true)
+
+-- Side HUD Options
+local onion_window_groupbox_3_groupbox_1 = gui.Groupbox(onion_window_groupbox_3, 'Side HUD Components', 0, 360)
+-- local onion_hud_side_options_stats = gui.Checkbox(onion_window_groupbox_3_groupbox_1, 'onion_hud_side_options_stats', 'Stats', true)
+local onion_hud_side_options_information = gui.Checkbox(onion_window_groupbox_3_groupbox_1, 'onion_hud_side_options_information', 'Information', true)
+local onion_hud_side_options_velocity = gui.Checkbox(onion_window_groupbox_3_groupbox_1, 'onion_hud_side_options_velocity', 'Velocity', true)
+local onion_hud_side_options_server = gui.Checkbox(onion_window_groupbox_3_groupbox_1, 'onion_hud_side_options_server', 'Server', true)
 
 -- Fonts
 local textFont = draw.CreateFont( "Tahoma", 16 )
+local sideTextFont = draw.CreateFont( "Tahoma", 15 )
 
 -- Misc Variables
 local scrW, scrH = 0, 0
@@ -56,7 +73,7 @@ local server = ""
 local maxVelocity
 local curVelocity
 local playerResources
-local curTime = "12:00"
+local curTime = ""
 local map
 local tick
 
@@ -149,12 +166,15 @@ end
 -- HUD Drawing Functions
 --
 
-function drawHUDBar()
+function drawTopHUD()
+	if (onion_hud_enabled_ingame:GetValue() == true and localPlayer == nil) then
+		return
+	end
+
 	local hudPosition = onion_hud_position:GetValue()
 	local hudStyle = onion_hud_style:GetValue()
 	local x, y = 0, 0
 	local r, g, b, a = onion_hud_color_text:GetValue()
-	local backR, backG, backB, backA = onion_hud_color_backcolor:GetValue()
 	local borderR, borderG, borderB, borderA = onion_hud_color_bordercolor:GetValue()
 	local border2R, border2G, border2B, border2A = onion_hud_color_bordercolor2:GetValue()
 	local hudString = "aimware | "
@@ -232,7 +252,7 @@ function drawHUDBar()
 	-- Draw the HUD
 	if (hudStyle == 0) then
 		drawFilledRect(borderR, borderG, borderB, borderA, x, y, hudW, 2)
-		drawFilledRect(backR, backG, backB, backA, x, y + 2, hudW, hudH - 2)
+		drawFilledRect(50, 50, 50, 185, x, y + 2, hudW, hudH - 2)
 		drawCenteredText(r, g, b, a, x + (hudW / 2), (y + 1) + ((hudH - 2) / 2), textFont, hudString)
 	elseif (hudStyle == 1) then
 		drawFilledRect(10, 10, 10, 255, x, y, hudW, hudH)
@@ -246,6 +266,115 @@ function drawHUDBar()
 		drawGradient({borderR, borderG, borderB, borderA}, {border2R, border2G, border2B, border2A}, x + (hudW / 2), y, hudW / 2, 2)
 		drawFilledRect(10, 10, 10, 125, x, y + 2, hudW, hudH - 2)
 		drawCenteredText(r, g, b, a, x + (hudW / 2), (y + 1) + ((hudH - 2) / 2), textFont, hudString)
+	end
+end
+
+function drawSideHUD()
+	if (onion_hud_side_enabled_ingame:GetValue() == true and localPlayer == nil) then
+		return
+	end
+	
+	local r, g, b, a = onion_hud_side_color_text:GetValue()
+	local borderR, borderG, borderB, borderA = onion_hud_side_color_bordercolor:GetValue()
+	local border2R, border2G, border2B, border2A = onion_hud_side_color_bordercolor2:GetValue()
+	local hudStyle = onion_hud_side_style:GetValue()
+	local x, y = onion_hud_side_position_x:GetValue(), (scrH / 2) + onion_hud_side_position_y:GetValue()
+	local usedY = 0
+	local addAmount = onion_hud_side_distance_y:GetValue()
+	
+	if (onion_hud_side_options_information:GetValue() == true) then
+		draw.Color(r, g, b, a)
+		draw.SetFont(sideTextFont)
+		local text = "Name: " .. username
+		local textW, textH = draw.GetTextSize(text)
+		
+		if (hudStyle == 0) then -- Sypher
+			drawFilledRect(borderR, borderG, borderB, borderA, x, y, textW + 20, 2)
+			drawFilledRect(50, 50, 50, 185, x, y + 2, textW + 20, ((textH + 10) - 2) - 2)
+			drawCenteredText(r, g, b, a, x + ((textW + 20) / 2), (y + 1) + (((textH + 10) - 2) / 2), sideTextFont, text)
+			usedY = usedY + addAmount + ((textH + 10) - 2)
+		elseif (hudStyle == 1) then -- Skeet
+			drawFilledRect(10, 10, 10, 255, x, y, textW + 20, textH + 20)
+			drawFilledRect(60, 60, 60, 255, x + 1, y + 1, textW + 18, textH + 18)
+			drawFilledRect(40, 40, 40, 255, x + 2, y + 2, textW + 16, textH + 16)
+			drawFilledRect(10, 10, 10, 255, x + 5, y + 5, textW + 10, textH + 10)
+			drawFilledRect(17, 17, 17, 255, x + 6, y + 6, textW + 8, textH + 8)
+			drawCenteredText(r, g, b, a, x + ((textW + 20) / 2), y + ((textH + 20) / 2), sideTextFont, text)
+			usedY = usedY + addAmount + (textH + 20)	
+		else -- Onetap
+			drawGradient({border2R, border2G, border2B, border2A}, {borderR, borderG, borderB, borderA}, x, y, (textW + 14) / 2, 2)
+			drawGradient({borderR, borderG, borderB, borderA}, {border2R, border2G, border2B, border2A}, x + ((textW + 14) / 2), y, (textW + 14) / 2, 2)
+			drawFilledRect(10, 10, 10, 125, x, y + 2, (textW + 14), (textH + 8) - 2)
+			drawCenteredText(r, g, b, a, x + ((textW + 14) / 2), (y + 1) + (((textH + 8) - 2) / 2), sideTextFont, text)
+			usedY = usedY + addAmount + (textH + 8)
+		end
+	end
+	
+	if (onion_hud_side_options_velocity:GetValue() == true) then
+		local text = curVelocity .. " m/s"
+		local textW, textH = draw.GetTextSize(text)
+		
+		if (hudStyle == 0) then -- Sypher
+			drawFilledRect(borderR, borderG, borderB, borderA, x, usedY + y, (scrW / 10) + 20, 2)
+			drawFilledRect(50, 50, 50, 185, x, usedY + y + 2, (scrW / 10) + 20, (((textH + 10) - 2) - 2) * 2)
+			drawCenteredText(r, g, b, a, x + (((scrW / 10) + 20) / 2), usedY + (y + 1) + (((textH + 10) - 2) / 2), sideTextFont, text)
+			drawFilledRect(60, 60, 60, 255, x + 12, usedY + (y + 15) + textH, ((scrW / 10) + 8) - 12, 2)
+			drawFilledRect(borderR, borderG, borderB, borderA, x + 12, usedY + (y + 15) + textH, (((scrW / 10) + 8) - 12) * (curVelocity / maxVelocity), 2)
+			usedY = usedY + addAmount + ((((textH + 10) - 2) - 2) * 2) + 2
+		elseif (hudStyle == 1) then -- Skeet
+			drawFilledRect(10, 10, 10, 255, x, usedY + y, (scrW / 10) + 20, textH + 26)
+			drawFilledRect(60, 60, 60, 255, x + 1, usedY + y + 1, (scrW / 10) + 18, textH + 24)
+			drawFilledRect(40, 40, 40, 255, x + 2, usedY + y + 2, (scrW / 10) + 16, textH + 22)
+			drawFilledRect(10, 10, 10, 255, x + 5, usedY + y + 5, (scrW / 10) + 10, textH + 16)
+			drawFilledRect(17, 17, 17, 255, x + 6, usedY + y + 6, (scrW / 10) + 8, textH + 14)
+			drawCenteredText(r, g, b, a, x + (((scrW / 10) + 20) / 2), usedY + y + ((textH + 20) / 2), sideTextFont, text)
+			drawFilledRect(60, 60, 60, 255, x + 12, usedY + (y + 15) + textH, ((scrW / 10) + 8) - 12, 2)
+			drawFilledRect(borderR, borderG, borderB, borderA, x + 12, usedY + (y + 15) + textH, (((scrW / 10) + 8) - 12) * (curVelocity / maxVelocity), 2)
+			usedY = usedY + addAmount + (textH + 26)
+		else -- Onetap
+			drawGradient({border2R, border2G, border2B, border2A}, {borderR, borderG, borderB, borderA}, x, usedY + y, ((scrW / 10) + 20) / 2, 2)
+			drawGradient({borderR, borderG, borderB, borderA}, {border2R, border2G, border2B, border2A}, x + (((scrW / 10) + 20) / 2), usedY + y, ((scrW / 10) + 20) / 2, 2)
+			drawFilledRect(10, 10, 10, 125, x, usedY + y + 2, (scrW / 10) + 20, (((textH + 10) - 2) - 2) * 2)
+			drawCenteredText(r, g, b, a, x + (((scrW / 10) + 20) / 2), usedY + (y + 1) + (((textH + 10) - 2) / 2), sideTextFont, text)
+			drawFilledRect(60, 60, 60, 255, x + 12, usedY + (y + 15) + textH, ((scrW / 10) + 8) - 12, 2)
+			drawFilledRect(borderR, borderG, borderB, borderA, x + 12, usedY + (y + 15) + textH, (((scrW / 10) + 8) - 12) * (curVelocity / maxVelocity), 2)
+			usedY = usedY + addAmount + ((((textH + 10) - 2) - 2) * 2) + 2
+		end
+	end
+	
+	if (onion_hud_side_options_server:GetValue() == true) then
+		local textW, textH = draw.GetTextSize(server)
+		local textW2, textH2 = draw.GetTextSize(ping)
+		
+		if (textW2 > textW) then
+			textW = textW2
+		end
+		
+		if (textH2 > textH) then
+			textH = textH2
+		end
+		
+		if (hudStyle == 0) then -- Sypher
+			drawFilledRect(borderR, borderG, borderB, borderA, x, usedY + y, textW + 10, 2)
+			drawFilledRect(50, 50, 50, 185, x, usedY + y + 2, textW + 10, ((textH + 8) * 2) - 2)
+			drawCenteredText(r, g, b, a, x + ((textW + 10) / 2), usedY + y + 2 + ((textH + 8) / 2), sideTextFont, server)
+			drawCenteredText(r, g, b, a, x + ((textW + 10) / 2), usedY + y + 2 + ((textH + 8) / 2) + (textH + 6), sideTextFont, ping)
+		elseif (hudStyle == 1) then -- Skeet
+			drawFilledRect(10, 10, 10, 255, x, usedY + y, textW + 20, ((textH + 8) * 2) + 12)
+			drawFilledRect(60, 60, 60, 255, x + 1, usedY + y + 1, textW + 18, ((textH + 8) * 2) + 10)
+			drawFilledRect(40, 40, 40, 255, x + 2, usedY + y + 2, textW + 16, ((textH + 8) * 2) + 8)
+			drawFilledRect(10, 10, 10, 255, x + 5, usedY + y + 5, textW + 10, ((textH + 8) * 2) + 2)
+			drawFilledRect(17, 17, 17, 255, x + 6, usedY + y + 6, textW + 8, (textH + 8) * 2)
+			drawCenteredText(r, g, b, a, x + ((textW + 20) / 2), usedY + y + ((textH + 20) / 2), sideTextFont, server)
+			drawCenteredText(r, g, b, a, x + ((textW + 20) / 2), usedY + y + ((textH + 20) / 2) + (textH + 8), sideTextFont, ping)
+			usedY = usedY + addAmount + (((textH + 8) * 2) + 12)
+		else -- Onetap
+			drawGradient({border2R, border2G, border2B, border2A}, {borderR, borderG, borderB, borderA}, x, usedY + y, (textW + 10) / 2, 2)
+			drawGradient({borderR, borderG, borderB, borderA}, {border2R, border2G, border2B, border2A}, x + ((textW + 10) / 2), usedY + y, (textW + 10) / 2, 2)
+			drawFilledRect(10, 10, 10, 125, x, usedY + y + 2, textW + 10, ((textH + 8) * 2) - 2)
+			drawCenteredText(r, g, b, a, x + ((textW + 10) / 2), usedY + y + 2 + ((textH + 8) / 2), sideTextFont, server)
+			drawCenteredText(r, g, b, a, x + ((textW + 10) / 2), usedY + y + 2 + ((textH + 8) / 2) + (textH + 6), sideTextFont, ping)
+		end
 	end
 end
 
@@ -306,7 +435,8 @@ function drawHUD()
 	end
 	
 	if (onion_hud_enabled:GetValue() == true) then
-		drawHUDBar()
+		drawTopHUD()
+		drawSideHUD()
 	end
 end
 
